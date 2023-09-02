065e5715f3b751aa39230b483ed0871b7fcc8445
BuildTools <unconfigured@null.spigotmc.org>
2023-09-03 02:29:28 +0800

    chore: added ci/cd deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NVM_DIR=$HOME/.nvm;
+. $NVM_DIR/nvm.sh;
+
+cd /home/ec2-user/knddras-strapi
+git pull origin develop
+nvm use v18
+yarn install
+yarn build
+pm2 resurrect
+pm2 restart knddras-strapi
